ab3daf527d04d2d775fa8041917af50079e2a6e2
wayzammit-collab <way.zammit@gmail.com>
2025-09-19 23:31:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base:"ButtonOpenStandaloneV1/",
+  base:"/ButtonOpenStandaloneV1/",
   server: {
     port: 5173,
     open: true
